26b0b0411ad729015194b9f0c833d30be26e6f4e
Diana <5275194+DianaNites@users.noreply.github.com>
2022-11-23 13:52:32 -0800

    wip

---
+++ src/error.rs
@@ -0,0 +1,23 @@
+//! Error types
+use core::{fmt, fmt::Write};
+pub type Result<T> = core::result::Result<T, Error>;
+
+/// NuPe Error type
+#[derive(Debug)]
+pub enum Error {
+    InvalidDosMagic,
+    InvalidPeMagic,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
