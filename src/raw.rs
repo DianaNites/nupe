e0be98aa5c64a712857f18c077643e39547e3bf9
Diana <5275194+DianaNites@users.noreply.github.com>
2023-04-16 13:46:04 -0700

    Move RawCoff to own file

--- src/raw.rs
+++ src/raw.rs
@@ -42,10 +42,12 @@ use crate::{
     Subsystem,
 };
 
+pub mod coff;
 pub mod dos;
 pub mod rich;
 
-pub(super) use dos::*;
+pub use coff::*;
+pub use dos::*;
 
 /// PE COFF Magic signature
 pub const PE_MAGIC: &[u8] = b"PE\0\0";
@@ -56,115 +58,6 @@ pub const PE32_MAGIC: u16 = 0x10B;
 /// PE32+ Magic signature
 pub const PE32_64_MAGIC: u16 = 0x20B;
 
-/// Raw COFF header
-///
-/// This is common to both executable PE images and object files.
-#[derive(Debug, Clone, Copy)]
-#[repr(C, packed)]
-pub struct RawCoff {
-    /// Target machine type
-    pub machine: MachineType,
-
-    /// Number of sections
-    pub sections: u16,
-
-    /// Timestamp
-    pub time: u32,
-
-    /// File offset to COFF symbol table
-    ///
-    /// According to MS, should be zero for images, as COFF debugging info
-    /// is deprecated.
-    pub sym_offset: u32,
-
-    /// Number of entries in COFF symbol table
-    ///
-    /// According to MS, should be zero for images, as COFF debugging info
-    /// is deprecated.
-    pub sym_len: u32,
-
-    /// Claimed size in bytes of the exec Header.
-    ///
-    /// Required for executable PE images
-    ///
-    /// Should be zero for object files, but is not required to be.
-    ///
-    /// Otherwise known as the "optional" header
-    pub exec_header_size: u16,
-
-    /// Object attributes
-    pub file_attributes: CoffFlags,
-}
-
-/// Public deserialization API
-impl RawCoff {
-    /// Create a new COFF header
-    ///
-    /// The deprecated COFF debugging information is set to zero
-    pub fn new(
-        machine: MachineType,
-        sections: u16,
-        time: u32,
-        optional_size: u16,
-        attributes: CoffFlags,
-    ) -> Self {
-        Self {
-            machine,
-            sections,
-            time,
-            sym_offset: 0,
-            sym_len: 0,
-            exec_header_size: optional_size,
-            file_attributes: attributes,
-        }
-    }
-
-    /// Get a [`RawCoff`] from a pointer to a COFF header
-    ///
-    /// This function validates that `size` is enough to contain this header
-    ///
-    /// # Errors
-    ///
-    /// - [`Error::NotEnoughData`] If `size` is not enough to fit a [`RawCoff`]
-    ///
-    /// # Safety
-    ///
-    /// - `data` MUST be valid for `size` bytes.
-    /// - You must ensure the returned reference does not outlive `data`, and is
-    ///   not mutated for the duration of lifetime `'data`.
-    pub unsafe fn from_ptr<'data>(data: *const u8, size: usize) -> Result<&'data Self> {
-        Ok(&*(Self::from_ptr_internal(data, size)?))
-    }
-
-    /// Get a mutable [`RawCoff`] from a pointer to a COFF header
-    ///
-    /// See [`RawCoff::from_ptr`] for error information and other details.
-    ///
-    /// # Safety
-    ///
-    /// - `data` MUST be valid for `size` bytes.
-    /// - You MUST ensure NO OTHER references exist when you call this.
-    /// - No instance of `&Self` can exist at the moment of this call.
-    /// - You must ensure the returned reference does not outlive `data`
-    pub unsafe fn from_ptr_mut<'data>(data: *mut u8, size: usize) -> Result<&'data mut Self> {
-        Ok(&mut *(Self::from_ptr_internal(data.cast_const(), size)?).cast_mut())
-    }
-}
-
-/// Internal base API
-impl RawCoff {
-    /// See [`RawCoff::from_ptr`]
-    fn from_ptr_internal(data: *const u8, size: usize) -> Result<*const Self> {
-        debug_assert!(!data.is_null(), "`data` was null in RawCoff::from_ptr");
-
-        // Ensure that size is enough
-        size.checked_sub(size_of::<RawCoff>())
-            .ok_or(Error::NotEnoughData)?;
-
-        Ok(data.cast())
-    }
-}
-
 /// Microsoft PE Signature and COFF header, assumed to be an executable image.
 #[derive(Clone, Copy)]
 #[repr(C, packed)]
@@ -173,7 +66,7 @@ pub struct RawPe {
     pub sig: [u8; 4],
 
     /// COFF file header
-    pub coff: RawCoff,
+    pub coff: coff::RawCoff,
 }
 
 impl RawPe {
@@ -699,6 +592,8 @@ impl RawDataDirectory {
 ///
 /// An ideal file upholds this. An in the wild file may not, and yet still run.
 /// It must thus also be parsed.
+///
+/// [`RawCoff::sections`]: coff::RawCoff::sections
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(C, packed)]
 pub struct RawSectionHeader {
@@ -833,6 +728,7 @@ impl fmt::Debug for RawSectionHeader {
 mod tests {
     use core::mem::align_of;
 
+    use coff::RawCoff;
     use dos::RawDos;
     use static_assertions::{assert_eq_size, const_assert_eq};
 
---
+++ src/raw/coff.rs
@@ -0,0 +1,124 @@
+//! The COFF header
+//!
+//! The Microsoft COFF header is shared between object files and executables
+//!
+//! In the context of PE executables, most COFF features are unused.
+use core::{fmt, mem::size_of};
+
+use crate::{
+    error::{Error, Result},
+    CoffFlags,
+    ExecFlags,
+    MachineType,
+    SectionFlags,
+    Subsystem,
+};
+
+/// Raw COFF header
+///
+/// This is common to both executable PE images and object files.
+#[derive(Debug, Clone, Copy)]
+#[repr(C, packed)]
+pub struct RawCoff {
+    /// Target machine type
+    pub machine: MachineType,
+
+    /// Number of sections
+    pub sections: u16,
+
+    /// Timestamp
+    pub time: u32,
+
+    /// File offset to COFF symbol table
+    ///
+    /// According to MS, should be zero for images, as COFF debugging info
+    /// is deprecated.
+    pub sym_offset: u32,
+
+    /// Number of entries in COFF symbol table
+    ///
+    /// According to MS, should be zero for images, as COFF debugging info
+    /// is deprecated.
+    pub sym_len: u32,
+
+    /// Claimed size in bytes of the exec Header.
+    ///
+    /// Required for executable PE images
+    ///
+    /// Should be zero for object files, but is not required to be.
+    ///
+    /// Otherwise known as the "optional" header
+    pub exec_header_size: u16,
+
+    /// Object attributes
+    pub file_attributes: CoffFlags,
+}
+
+/// Public deserialization API
+impl RawCoff {
+    /// Create a new COFF header
+    ///
+    /// The deprecated COFF debugging information is set to zero
+    pub fn new(
+        machine: MachineType,
+        sections: u16,
+        time: u32,
+        optional_size: u16,
+        attributes: CoffFlags,
+    ) -> Self {
+        Self {
+            machine,
+            sections,
+            time,
+            sym_offset: 0,
+            sym_len: 0,
+            exec_header_size: optional_size,
+            file_attributes: attributes,
+        }
+    }
+
+    /// Get a [`RawCoff`] from a pointer to a COFF header
+    ///
+    /// This function validates that `size` is enough to contain this header
+    ///
+    /// # Errors
+    ///
+    /// - [`Error::NotEnoughData`] If `size` is not enough to fit a [`RawCoff`]
+    ///
+    /// # Safety
+    ///
+    /// - `data` MUST be valid for `size` bytes.
+    /// - You must ensure the returned reference does not outlive `data`, and is
+    ///   not mutated for the duration of lifetime `'data`.
+    pub unsafe fn from_ptr<'data>(data: *const u8, size: usize) -> Result<&'data Self> {
+        Ok(&*(Self::from_ptr_internal(data, size)?))
+    }
+
+    /// Get a mutable [`RawCoff`] from a pointer to a COFF header
+    ///
+    /// See [`RawCoff::from_ptr`] for error information and other details.
+    ///
+    /// # Safety
+    ///
+    /// - `data` MUST be valid for `size` bytes.
+    /// - You MUST ensure NO OTHER references exist when you call this.
+    /// - No instance of `&Self` can exist at the moment of this call.
+    /// - You must ensure the returned reference does not outlive `data`
+    pub unsafe fn from_ptr_mut<'data>(data: *mut u8, size: usize) -> Result<&'data mut Self> {
+        Ok(&mut *(Self::from_ptr_internal(data.cast_const(), size)?).cast_mut())
+    }
+}
+
+/// Internal base API
+impl RawCoff {
+    /// See [`RawCoff::from_ptr`]
+    fn from_ptr_internal(data: *const u8, size: usize) -> Result<*const Self> {
+        debug_assert!(!data.is_null(), "`data` was null in RawCoff::from_ptr");
+
+        // Ensure that size is enough
+        size.checked_sub(size_of::<RawCoff>())
+            .ok_or(Error::NotEnoughData)?;
+
+        Ok(data.cast())
+    }
+}
